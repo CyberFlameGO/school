a2c2a60a4d7f6ac93d9a25170d2a6a9e8b6e31ec
Jake Ward <thegaminggeek362@gmail.com>
2019-04-29 14:17:16 +0100

    Hangman drawing progress

--- hangman.py
+++ hangman.py
@@ -5,17 +5,28 @@ wordneeded = True
 choice = None
 
 def getwords():
-    words = r.get('https://raw.githubusercontent.com/first20hours/google-10000-english/master/20k.txt') #TODO Change URL to simpler words
-    wordlist = words.text
-    return random.choice(wordlist.split('\n'))
+	words = r.get('https://raw.githubusercontent.com/first20hours/google-10000-english/master/20k.txt') #TODO Change URL to simpler words
+	wordlist = words.text
+	return random.choice(wordlist.split('\n'))
 
 while wordneeded:
-    choice = getwords()
-    if len(choice) < 5:
-        pass
-    elif len(choice) > 12:
-        pass
-    else:
-        wordneeded = False
+	choice = getwords()
+	if len(choice) < 5:
+		pass
+	elif len(choice) > 12:
+		pass
+	else:
+		wordneeded = False
+
+
+Stages = {
+	1: "   ____     \n  |    |    \n  |         \n  |         \n  |         \n  |         \n _|_        \n|   |______ \n|          |\n|__________|",
+	2: "   ____     \n  |    |    \n  |    o    \n  |         \n  |         \n  |         \n _|_        \n|   |______ \n|          |\n|__________|",
+	3: "   ____     \n  |    |    \n  |    o    \n  |   /     \n  |         \n  |         \n _|_        \n|   |______ \n|          |\n|__________|",
+	4: "   ____     \n  |    |    \n  |    o    \n  |   /|    \n  |         \n  |         \n _|_        \n|   |______ \n|          |\n|__________|",
+	5: "   ____     \n  |    |    \n  |    o    \n  |   /|\   \n  |         \n  |         \n _|_        \n|   |______ \n|          |\n|__________|",
+	6: "   ____     \n  |    |    \n  |    o    \n  |   /|\   \n  |    |    \n  |         \n _|_        \n|   |______ \n|          |\n|__________|",
+	7: ""
+}
 
 print(choice)
