f1fdd59ef480f1c6ff01297cb7deccdce2a4208d
Jake Ward <thegaminggeek362@gmail.com>
2019-04-29 14:25:20 +0100

    Start guessing system

--- hangman.py
+++ hangman.py
@@ -3,6 +3,8 @@ import random
 
 wordneeded = True
 choice = None
+stage = 0
+guessed = []
 
 def getwords():
 	words = r.get('https://raw.githubusercontent.com/first20hours/google-10000-english/master/20k.txt') #TODO Change URL to simpler words
@@ -29,5 +31,13 @@ Stages = {
 	7: "   ____     \n  |    |    \n  |    o    \n  |   /|\   \n  |    |    \n  |   /     \n _|_        \n|   |______ \n|          |\n|__________|",
 	8: "   ____     \n  |    |    \n  |    o    \n  |   /|\   \n  |    |    \n  |   / \   \n _|_        \n   |______ \n|          |\n|__________|"
 }
+
+while True:
+	if stage == 0:
+		letter = input('Guess a letter!')
+		if letter in guessed:
+			print('You\'ve already guessed this letter')
+		elif letter in choice:
+			guessed.append(letter)
 
 print(choice)
