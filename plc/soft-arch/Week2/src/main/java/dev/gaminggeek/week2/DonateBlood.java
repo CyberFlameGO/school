571e805d801984932e11dbce3ee52d2448173f01
Jake Ward <geek@gaminggeek.dev>
2021-10-06 11:19:57 +0000

    add new file

---
+++ plc/soft-arch/Week2/src/main/java/dev/gaminggeek/week2/DonateBlood.java
@@ -0,0 +1,28 @@
+package dev.gaminggeek.week2;
+
+import java.util.Scanner;
+
+
+public class DonateBlood {
+  public static void main(String []args) {
+    int age, weight;
+
+    Scanner scanner = new Scanner(System.in);
+
+    System.out.println("Enter your age:");
+    age = scanner.nextInt();
+
+    if (age < 18) {
+      System.out.println("You must be at least 18 to donate blood.");
+      return;
+    }
+
+    System.out.println("Enter your weight:");
+    weight = scanner.nextInt();
+
+    if (age >= 18 && weight > 50)
+      System.out.println("You are eligible to donate blood!");
+    else
+      System.out.println("Weight must be greater than 50 to donate blood.");
+  }
+}
