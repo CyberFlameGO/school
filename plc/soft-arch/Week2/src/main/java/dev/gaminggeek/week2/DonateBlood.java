06d48d514ba2b67f5535d7320c21142bd0fd9de3
Jake Ward <geek@gaminggeek.dev>
2021-10-07 10:54:21 +0000

    take existing scanner instead of empty array

--- plc/soft-arch/Week2/src/main/java/dev/gaminggeek/week2/DonateBlood.java
+++ plc/soft-arch/Week2/src/main/java/dev/gaminggeek/week2/DonateBlood.java
@@ -4,10 +4,8 @@ import java.util.Scanner;
 
 
 public class DonateBlood {
-  public static void main(String []args) {
+  public static void main(Scanner scanner) {
     int age, weight;
-
-    Scanner scanner = new Scanner(System.in);
 
     System.out.println("Enter your age:");
     age = scanner.nextInt();
