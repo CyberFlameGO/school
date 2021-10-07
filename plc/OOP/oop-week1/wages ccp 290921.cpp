2c38e03613c674b616799815bd2197746dff712a
Jake Ward <geek@gaminggeek.dev>
2021-10-07 12:25:53 +0000

    stuff

--- plc/OOP/oop-week1/wages ccp 290921.cpp
+++ plc/OOP/oop-week1/wages ccp 290921.cpp
@@ -1,39 +1,44 @@
 #include <iostream>
+#include <iomanip>
 
 using namespace std;
 /// DECLARE VARIABLES
 /// TYPE OF VARIABLE   VARIABLE_NAME;
-int rate = 15;
 float net_pay;
 float hours;
 float gross;
 float tax;
+int rate;
 
 int main()
 {
 
-    cout << "Wages Calculation" << endl;
-    cout << "Key in hours worked " << endl;
+    cout << "Wages Calculation" << endl << endl;
 
+
+    cout << "Enter amount of hours worked" << endl;
     cin >> hours;
+
+    cout << "Enter hourly rate" << endl;
+    cin >> rate;
 
     // FORMAT FOR CALCULATIONS
     // RESULT_VARIABLE = VARIABLE1  * VARIABLE2;
     gross = hours * rate;
-    cout << "Gross pay = " << gross << endl;
+    cout << "Gross Pay = " << fixed << setprecision(2) << gross << endl;
 
     if (gross <= 650)
     {
         tax = gross * .2;
     }
-    else if (gross > 650)
+    else
     {
-        // add smth here eventually
+        tax = ((gross - 650) * .4) + 130;
     }
-    cout << "Tax = " << tax << endl;
+    cout << "PAYE" << fixed << setprecision(2) << setw(8) << " = " << tax << endl;
 
     net_pay = gross - tax;
-    cout << "Net pay = " << net_pay << endl;
+    cout << "Net Pay" << fixed << setprecision(2) << setw(5) << " = " << net_pay << endl;
 
     return 0;
 }
